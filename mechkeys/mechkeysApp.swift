8c8aa1262057627ca09d93644c3b243b4d2376fb
n1x9s <n1x9s@icloud.com>
2026-02-02 13:49:56 +0300

    Initial Commit

---
+++ mechkeys/mechkeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  mechkeysApp.swift
+//  mechkeys
+//
+//  Created by Nikita Sergeev on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct mechkeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
